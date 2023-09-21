5c7d4a17b98adf877b89a40789dca842575433f2
nanahuse <109294061+Nanahuse@users.noreply.github.com>
2023-09-21 23:14:46 +1200

    fix: update setup detail

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyphonixjp",
-    version="1.0.0",
+    version="1.1",
     packages=find_packages(),
     install_requires=[],
+    license="MIT",
+    url="https://github.com/Nanahuse/PyPhonixJP",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
